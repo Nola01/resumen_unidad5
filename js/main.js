076eb49d6f968498239962bea63df5593ceee9d1
Nola <juanmanolasco0105@gmail.com>
2021-03-14 23:16:28 +0100

    portfolio

---
+++ js/main.js
@@ -0,0 +1,66 @@
+const grid = new Muuri('.grid', {
+	layout: {
+		rounding: false
+	}
+});
+
+window.addEventListener('load', () => {
+	grid.refreshItems().layout();
+	document.getElementById('grid').classList.add('imagenes-cargadas');
+
+	// Agregamos los listener de los enlaces para filtrar por categoria.
+	const enlaces = document.querySelectorAll('#categorias a');
+	enlaces.forEach((elemento) => {
+		elemento.addEventListener('click', (evento) => {
+			evento.preventDefault();
+			enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+			evento.target.classList.add('activo');
+
+			const categoria = evento.target.innerHTML.toLowerCase();
+			grid.filter(`[data-categoria="${categoria}"]`);
+		});
+	});
+
+	// Agregamos el listener para la barra de busqueda
+	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
+		const busqueda = evento.target.value;
+		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+	});
+
+	// Agregamos listener para las imagenes
+	const overlay = document.getElementById('overlay');
+	document.querySelectorAll('.grid .item img').forEach((elemento) => {
+		elemento.addEventListener('click', () => {
+			const ruta = elemento.getAttribute('src');
+			const descripcion = elemento.parentNode.parentNode.dataset.descripcion;
+
+			overlay.classList.add('activo');
+			document.querySelector('#overlay img').src = ruta;
+			document.querySelector('#overlay .descripcion').innerHTML = descripcion;
+		});
+	});
+
+	// Eventlistener del boton de cerrar
+	document.querySelector('#btn-cerrar-popup').addEventListener('click', () => {
+		overlay.classList.remove('activo');
+	});
+
+	// Eventlistener del overlay
+	overlay.addEventListener('click', (evento) => {
+		evento.target.id === 'overlay' ? overlay.classList.remove('activo') : '';
+	});
+
+});
+
+/* Función que nos muestra un mensaje de alerta al pulsar sobre las opciones que no funcionan todavía. 
+function noDisponible() {
+    let texto1 = "Estamos trabjando en ello..."
+
+    swal("Función no disponible","(Estamos trabajando en ello... 🛠️💻)", "warning")
+	}
+*/
+
+/*function noDisponible() {
+	window.alert("Estamos trabajando en ello...");
+	alert("Estamos trabajando en ello...");
+}*/
